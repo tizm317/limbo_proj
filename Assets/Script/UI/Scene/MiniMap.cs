acc1fa8ff6ea3587e99b069e23ae79b73683dcbe
HOYOUNG LEE <tizm32@naver.com>
2022-05-10 14:48:02 +0900

    주석처리 + 이상한 에셋들 부활하는거(걍 충돌나면 저장소꺼로 하던지 자기꺼로 하던지 상관없을거 같기도?)

--- Assets/Script/UI/Scene/MiniMap.cs
+++ Assets/Script/UI/Scene/MiniMap.cs
@@ -43,26 +43,26 @@ public class MiniMap : UI_Scene
     {
         // 플레이어 위치(파란색), 목적지(빨간색) 표시
 
-        Vector3 playerPos = player.position;
-        Vector3 destination = player_Controller.destination;
-        float dist = Mathf.Abs(playerPos.magnitude - destination.magnitude);
+        //Vector3 playerPos = player.position;
+        //Vector3 destination = player_Controller.destination;
+        //float dist = Mathf.Abs(playerPos.magnitude - destination.magnitude);
 
-        // 여기에 플레이어가 이동중인지를 확인하는 것도 괜찮을듯
-        // 일정 거리 이내면 목적지 표시 x
-        if (dist < 1.0f)
-            destinationImage.gameObject.SetActive(false);
-        else
-            destinationImage.gameObject.SetActive(true);
+        //// 여기에 플레이어가 이동중인지를 확인하는 것도 괜찮을듯
+        //// 일정 거리 이내면 목적지 표시 x
+        //if (dist < 1.0f)
+        //    destinationImage.gameObject.SetActive(false);
+        //else
+        //    destinationImage.gameObject.SetActive(true);
 
-        // 플레이어
-        playerPos.y = player.position.z;
-        playerPos.z = 0;
-        playerImage.localPosition = playerPos;
+        //// 플레이어
+        //playerPos.y = player.position.z;
+        //playerPos.z = 0;
+        //playerImage.localPosition = playerPos;
 
-        // 목적지
-        destination.y = player_Controller.destination.z;
-        destination.z = 0;
-        destinationImage.localPosition = destination;
+        //// 목적지
+        //destination.y = player_Controller.destination.z;
+        //destination.z = 0;
+        //destinationImage.localPosition = destination;
 
         // 반대로 미니맵 클릭해서 이동하는 방식도 있으면 좋을듯?
         // UI 에 레이케스트 해서 
@@ -73,24 +73,24 @@ public class MiniMap : UI_Scene
     {
         base.Init();
 
-        Bind<GameObject>(typeof(GameObjects));
+        //Bind<GameObject>(typeof(GameObjects));
 
-        player = GameObject.Find("Player").transform;
+        //player = GameObject.Find("Player").transform;
 
-        //InputManager = Managers._Input;
+        ////InputManager = Managers._Input;
 
-        inputManager = GameObject.Find("InputManager");
-        player_Controller = inputManager.GetComponent<Player_Controller>();
+        //inputManager = GameObject.Find("InputManager");
+        //player_Controller = inputManager.GetComponent<Player_Controller>();
 
-        //GameObject playerImage = Get<GameObject>((int)GameObjects.PlayerImage);
+        ////GameObject playerImage = Get<GameObject>((int)GameObjects.PlayerImage);
 
-        Vector3 temp = new Vector3(0, 0, 0);
-        //
-        GameObject go = GetObject((int)GameObjects.MapImage).gameObject;
-        GameObject destinationImage = GetObject((int)GameObjects.DestinationImage).gameObject;
-        BindEvent(go, (PointerEventData data) => { destinationImage.transform.localPosition = new Vector3 (data.position.x - 784.375f, data.position.y - 316.25f, 0); 
-            temp.z = destinationImage.transform.localPosition.y;
-            temp.x = destinationImage.transform.localPosition.x;
-            player_Controller.Set_Destination(temp); }, Define.UIEvent.Click);
+        //Vector3 temp = new Vector3(0, 0, 0);
+        ////
+        //GameObject go = GetObject((int)GameObjects.MapImage).gameObject;
+        //GameObject destinationImage = GetObject((int)GameObjects.DestinationImage).gameObject;
+        //BindEvent(go, (PointerEventData data) => { destinationImage.transform.localPosition = new Vector3 (data.position.x - 784.375f, data.position.y - 316.25f, 0); 
+        //    temp.z = destinationImage.transform.localPosition.y;
+        //    temp.x = destinationImage.transform.localPosition.x;
+        //    player_Controller.Set_Destination(temp); }, Define.UIEvent.Click);
     }
 }
