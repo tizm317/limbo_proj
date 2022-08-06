8f43f632f9703fbdf72d6a9d7cbc8492eb4e7a45
yeongchankim <b611040@g.hongik.ac.kr>
2022-08-06 19:44:48 +0900

    Revert "HY) 장비창 UI 카메라 회전"
    This reverts commit 863dcd051c030ae1bc71a98462dfc8d0525399ef.

--- Assets/Script/UI/Popup/UI_Equipment.cs
+++ Assets/Script/UI/Popup/UI_Equipment.cs
@@ -6,12 +6,6 @@ using UnityEngine.UI;
 
 public class UI_Equipment : UI_Popup
 {
-    Transform player;
-    Camera selfCam;
-    float x_cam;
-    float z_cam;
-    float r;
-
     enum GameObjects
     {
         Slider,
@@ -28,41 +22,31 @@ public class UI_Equipment : UI_Popup
         base.Init();
         Bind<GameObject>(typeof(GameObjects));
         GetObject((int)GameObjects.Slider).BindEvent(onSliderDrag, Define.UIEvent.Drag);
-
-        player = GameObject.Find("Player").transform;
-        selfCam = player.Find("SelfCamera").GetComponent<Camera>();
-        selfCam.gameObject.SetActive(true);
-
-        // 캠 위치,방향 초기화
-        selfCam.transform.localPosition = new Vector3(0f, 1.2f, 3f);
-        selfCam.transform.LookAt(player.GetChild(0)); // Hips 기준
-
-        // r 계산
-        x_cam = selfCam.transform.position.x;
-        z_cam = selfCam.transform.position.z;
-        r = Vector2.Distance(new Vector2(x_cam, z_cam), new Vector2(player.position.x, player.position.z));
     }
 
     public void onSliderDrag(PointerEventData data)
     {
-        // 카메라 회전
-        x_cam = selfCam.transform.position.x;
-        z_cam = selfCam.transform.position.z;
+        Transform player = GameObject.Find("Player").transform;
+        Camera selfCam = player.gameObject.GetComponentInChildren<Camera>();
+        float x_cam = selfCam.transform.position.x;
+        float z_cam = selfCam.transform.position.z;
+
+        float r = Vector2.Distance(new Vector2(x_cam, z_cam), new Vector2(player.position.x, player.position.z));
 
         float sValue = GetObject((int)GameObjects.Slider).GetComponent<Slider>().value; // 0 ~ 1
-
-        x_cam = player.position.x +  r * Mathf.Sin(sValue * 2 * Mathf.PI);
-        if (sValue > 0.25f && sValue <= 0.75f)
+        // -r r -r
+        if (sValue < 0.5f)
+        {
+            x_cam = player.position.x + sValue * 4 * r - r;
             z_cam = player.position.z + Mathf.Sqrt(r * r - (x_cam - player.position.x) * (x_cam - player.position.x));
+        }
         else
+        {
+            x_cam = player.position.x + 3 * r - sValue * 4 * r;
             z_cam = player.position.z - Mathf.Sqrt(r * r - (x_cam - player.position.x) * (x_cam - player.position.x));
+        }
 
         selfCam.transform.position = new Vector3(x_cam, selfCam.transform.position.y, z_cam);
         selfCam.transform.LookAt(player.GetChild(0)); // Hips 기준
-    }
-
-    private void OnDisable()
-    {
-        selfCam.gameObject.SetActive(false);
     }
 }
