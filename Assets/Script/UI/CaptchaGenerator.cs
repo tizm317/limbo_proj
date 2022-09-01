afb3191830b7face4330366160123ca8c1beea92
HOYOUNG LEE <tizm32@naver.com>
2022-09-01 20:16:43 +0900

    HY) 매크로 방지 CAPTCHA 시스템 (코드 정리 필요)

---
+++ Assets/Script/UI/CaptchaGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CaptchaGenerator : ScriptableObject
+{
+    public CaptchaAlphabets[] alphabets;
+
+    //public static int Index = 0;
+    public int Count = 0;
+
+    public CaptchaAlphabets[] Generate()
+    {
+        CaptchaAlphabets[] Captcha = new CaptchaAlphabets[Count];
+        for (int i = 0; i < Count; i++)
+        {
+            int idx = Random.Range(0, 26);
+            Captcha[i] = alphabets[idx];
+        }
+        return Captcha;
+    }
+
+    public bool IsCodeValid(string input, CaptchaAlphabets[] captcha)
+    {
+        int i = 0;
+        string answer = "";
+
+        if (input.Length == 0)
+            return false;
+        
+        // 대소문자 구분X
+        input = input.ToUpper();
+
+        foreach (char c in input)
+        {
+            answer += captcha[i].Value.ToString();
+
+            if (captcha[i] == null || c != captcha[i++].Value )
+                return false;
+        }
+
+        Debug.Log($"Input : {input}");
+        Debug.Log($"Captcha : {answer}");
+        return true;
+    }
+}
---
+++ Assets/Script/UI/UI_Captcha.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Captcha : MonoBehaviour
+{
+    [Header("UI References :")]
+    [SerializeField] private Image[] uiCodeImage;
+    [SerializeField] private InputField uiCodeInput;
+    [SerializeField] private Text uiErrorsText;
+    [SerializeField] private Button uiRefreshButton;
+    [SerializeField] private Button uiSubmitButton;
+
+    [Header("Captcha Generator :")]
+    [SerializeField] private CaptchaGenerator captchaGenerator;
+
+
+    [SerializeField] private int Count;
+
+    private CaptchaAlphabets[] currentCaptcha;
+
+
+    void Start()
+    {
+        GenerateCaptcha();
+
+        //Buttons:
+        uiRefreshButton.onClick.AddListener(GenerateCaptcha);
+        uiSubmitButton.onClick.AddListener(Submit);
+    }
+
+    private void GenerateCaptcha()
+    {
+        currentCaptcha = captchaGenerator.Generate();
+
+        //Change UI:
+        for(int i = 0; i < Count; i++)
+            uiCodeImage[i].sprite = currentCaptcha[i].Image;
+        uiErrorsText.gameObject.SetActive(false);
+
+    }
+
+    private void Submit()
+    {
+        string enteredCode = uiCodeInput.text;
+
+        if(captchaGenerator.IsCodeValid(enteredCode, currentCaptcha))
+        {
+            //valid
+            uiErrorsText.gameObject.SetActive(false);
+            Debug.Log("<color=green>Valid Code</color>");
+
+        }
+        else
+        {
+            //invalid
+            uiErrorsText.gameObject.SetActive(true);
+            Debug.Log("<color=red>Invalid Code</color>");
+        }
+    }
+
+}
