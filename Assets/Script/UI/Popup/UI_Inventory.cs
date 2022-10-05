cf7226323237e8955ace42cafaa5e1dd4f2563a8
HOYOUNG LEE <tizm32@naver.com>
2022-10-05 17:54:03 +0900

    HY) 인벤토리 작업중...

--- Assets/Script/UI/Popup/UI_Inventory.cs
+++ Assets/Script/UI/Popup/UI_Inventory.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -39,11 +40,118 @@ public class UI_Inventory : UI_Popup
         Bind<Button>(typeof(Buttons));
         Button buttonClose = GetButton((int)Buttons.ButtonClose);
         GetButton((int)Buttons.ButtonClose).gameObject.BindEvent(Quit_Inventory);
+
+
+        /**/
+        _gr = Util.GetOrAddComponent<GraphicRaycaster>(this.gameObject);
+        _ped = new PointerEventData(EventSystem.current);
+        _rrList = new List<RaycastResult>(10);
     }
 
 
     public void Quit_Inventory(PointerEventData data)
     {
         this.ClosePopupUI();
+    }
+
+
+    /* 아이템 드래그 앤 드롭 이동 */
+    private GraphicRaycaster _gr;
+    private PointerEventData _ped;
+    private List<RaycastResult> _rrList;
+
+    private UI_ItemSlot _beginDragSlot;         // 현재 드래그 시작한 슬롯
+    private Transform _beginDragIconTransform;  // 해당 슬롯의 아이콘 트랜스폼
+
+    private Vector3 _beginDragIconPoint;        // 드래그 시작시 슬롯 위치
+    private Vector3 _beginDragCursorPoint;      // 드래그 시작시 커서 위치
+    private int _beginDragSlotSiblingIndex;
+
+    private void Update()
+    {
+        _ped.position = Input.mousePosition;
+
+        OnPointerDown();
+        OnPointerDrag();
+        OnPointerUp();
+    }
+
+
+    private T RaycastAndGetFirstComponent<T>() where T : Component
+    {
+        _rrList.Clear();
+        _gr.Raycast(_ped, _rrList);
+        if (_rrList.Count == 0) return null;
+        return _rrList[0].gameObject.GetComponent<T>();
+    }
+
+    private void OnPointerDown()
+    {
+        if(Input.GetMouseButtonDown(0)) // left click
+        {
+            _beginDragSlot = RaycastAndGetFirstComponent<UI_ItemSlot>();
+
+            if (_beginDragSlot && _beginDragSlot.HasItem)
+            {
+                // 위치 기억
+                _beginDragIconTransform = _beginDragSlot.IconRect.transform;
+                _beginDragIconPoint = _beginDragIconTransform.position;
+                _beginDragCursorPoint = Input.mousePosition;
+
+                // 맨 위에 보이기
+                _beginDragSlotSiblingIndex = _beginDragSlot.transform.GetSiblingIndex();
+                _beginDragSlot.transform.SetAsLastSibling();
+            }
+            else _beginDragSlot = null;
+        }
+    }
+
+    private void OnPointerDrag()
+    {
+        if (!_beginDragSlot) return;
+        if(Input.GetMouseButton(0))
+        {
+            // 위치 이동
+            _beginDragIconTransform.position = _beginDragIconPoint + Input.mousePosition - _beginDragCursorPoint;
+        }
+    }
+
+    private void OnPointerUp()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            if(_beginDragSlot)
+            {
+                // 위치 복원?
+                _beginDragIconTransform.position = _beginDragIconPoint;
+                
+                // UI 순서 복원
+                _beginDragSlot.transform.SetSiblingIndex(_beginDragSlotSiblingIndex);
+
+                // 드래그 완료 처리 ?
+                EndDrag();
+
+                // 참조 제거
+                _beginDragSlot = null;
+                _beginDragIconTransform = null;
+            }
+        }
+    }
+
+    private void EndDrag()
+    {
+        UI_ItemSlot endDragSlot = RaycastAndGetFirstComponent<UI_ItemSlot>();
+
+        if(endDragSlot && endDragSlot.HasItem)
+        {
+            TrySwapItems(_beginDragSlot, endDragSlot);
+        }
+    }
+
+    private void TrySwapItems(UI_ItemSlot from, UI_ItemSlot to)
+    {
+        if (from == to) return;
+        from.SwapIcon(to);
+        //_inventory.Swap(from.Index, to.Index);
     }
 }
---
+++ Assets/Script/UI/UI_ItemSlot.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_ItemSlot : MonoBehaviour
+{
+    [SerializeField] private Image _iconImage;
+    [SerializeField] private Text _amountText;
+
+    public int Index { get; private set; }
+    public bool HasItem => (_iconImage.sprite != null);
+    public bool IsAccessible => (_isAccessibleSlot && _isAccessibleItem);
+
+    public RectTransform SlotRect => _slotRect;
+    public RectTransform IconRect => _iconRect;
+
+
+    private UI_Inventory _ui_inventory;
+
+    private RectTransform _slotRect;
+    private RectTransform _iconRect;
+
+    private GameObject _iconGo;
+    private GameObject _textGo;
+
+    private Image _slotImage;
+
+    private bool _isAccessibleSlot = true;
+    private bool _isAccessibleItem = true;
+
+    // 비활성화 슬롯, 아이콘 색상
+    private Color InaccessibleSlotColor = new Color(0.2f, 0.2f, 0.2f, 0.5f);
+    private Color InaccessibleIconColor = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+
+
+    /* Methods */
+    private void ShowIcon() => _iconGo.SetActive(true);
+    private void HideIcon() => _iconGo.SetActive(false);
+    private void ShowText() => _textGo.SetActive(true);
+    private void HideText() => _textGo.SetActive(false);
+
+    public void SetSlotIndex(int idx) => Index = idx;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        _ui_inventory = GetComponentInParent<UI_Inventory>();
+
+        _slotRect = GetComponent<RectTransform>();
+        _iconRect = _iconImage.rectTransform;
+
+        _iconGo = _iconRect.gameObject;
+        _textGo = _amountText.gameObject;
+
+        _slotImage = GetComponent<Image>();
+    }
+
+    // 슬롯 활성화 여부 설정(?)
+    public void SetSlotAccessibleState(bool value)
+    {
+        if (_isAccessibleSlot == value) return;
+        if (value)
+            _slotImage.color = Color.black;
+        else
+        {
+            _slotImage.color = InaccessibleSlotColor;
+            HideIcon();
+            HideText();
+        }
+
+        _isAccessibleSlot = value;
+    }
+
+    // 아이템 활성화 여부 설정
+    public void SetItemAccessibleState(bool value)
+    {
+        if (_isAccessibleItem == value) return;
+        if (value)
+        {
+            _iconImage.color = Color.white;
+            _amountText.color = Color.white;
+        }
+        else
+        {
+            _iconImage.color = InaccessibleIconColor;
+            _amountText.color = InaccessibleIconColor;
+        }
+
+        _isAccessibleItem = value;
+    }
+
+    // 다른 슬롯과 아이템 아이콘 교환
+    public void SwapIcon(UI_ItemSlot otherSlot)
+    {
+        if (!otherSlot) return;
+        if (this == otherSlot) return;
+        if (!this.IsAccessible) return;
+        if (!otherSlot.IsAccessible) return;
+
+        Sprite tempSprite = _iconImage.sprite;
+
+        if (otherSlot.HasItem) SetItem(otherSlot._iconImage.sprite); // 교환
+        else RemoveItem(); // 이동
+
+        otherSlot.SetItem(tempSprite);
+    }
+
+    // 슬롯에 아이템 등록
+    public void SetItem(Sprite tempSprite)
+    {
+        if (!tempSprite) RemoveItem();
+        else
+        {
+            _iconImage.sprite = tempSprite;
+            ShowIcon();
+        }
+    }
+
+    // 슬롯에서 아이템 제거
+    public void RemoveItem()
+    {
+        _iconImage.sprite = null;
+        HideIcon();
+        HideText();
+    }
+
+    // 아이템 개수 텍스트 설정(amount 1 이하면 텍스트 미표시)
+    public void SetItemAmount(int amount)
+    {
+        if (HasItem && amount > 1) ShowText();
+        else HideText();
+
+        _amountText.text = amount.ToString();
+    }
+}
