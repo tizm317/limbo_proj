7685bce2bdf9c2f8b7b9abb218bd9da29fb5d42e
Yeongchan <b611040@gmail.com>
2022-10-12 02:44:16 +0900

    내일 만나서 해야할 작업

---
+++ Assets/Script/Player/New_Version/PlayerMgr.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMgr//Managers가 만약 Ingame에서 생성되는 거라면? Instance로 추가, 아니라면 Singleton을 Awake에 추가해주어야함
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GetInfo();
+    }
+    void Start()
+    {
+        Init();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GetInfo()
+    {
+        //캐릭터 종류와 데이터를 받아오는 내용이 필요함
+    }
+
+    void Init()
+    {
+        //캐릭터 종류와 데이터에 따라서 새로운 Instantiate해주는 내용이 필요함
+    }
+}
