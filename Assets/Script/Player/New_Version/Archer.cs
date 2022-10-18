a2757a5bdeb7debfb63771c964a598b39ace3352
Yeongchan <b611040@gmail.com>
2022-10-19 01:06:00 +0900

    e스킬이랑 패시브 구현 끝

--- Assets/Script/Player/New_Version/Archer.cs
+++ Assets/Script/Player/New_Version/Archer.cs
@@ -81,7 +81,7 @@ public class Archer : Player
 
     public override void E()
     {
-        StartCoroutine(Archer_E());
+        StartCoroutine(Archer_E(60f,attackRange,15f));
     }
 
     public override void R()
@@ -91,7 +91,7 @@ public class Archer : Player
 
     public override void Passive()
     {
-        StartCoroutine(Passive());
+        StartCoroutine(Archer_Passive());
     }
 
     IEnumerator Archer_Q()
@@ -115,8 +115,6 @@ public class Archer : Player
                     {              
                         pos = hit.point;
                         player.transform.forward = new Vector3(pos.x - player.transform.position.x, 0, pos.z - player.transform.position.z).normalized;
-                        Debug.Log(player.transform.forward);
-                        Debug.Log(new Vector3(pos.x - player.transform.position.x, 0, pos.z - player.transform.position.z).normalized);
                         pos_selected = true;
                     }
                 }
@@ -135,7 +133,7 @@ public class Archer : Player
                 skill = HotKey.Q;
                 Ani_State_Change();
                 attackable = false;
-                yield return new WaitForSeconds(1.633f);
+                yield return new WaitForSeconds(1.08f);
                 attackable = true;
                 
                 cool[0] = cool_max[0];
@@ -159,9 +157,94 @@ public class Archer : Player
         my_stat.AttackSpeed -= speed;
     }
 
-    IEnumerator Archer_E()
+    IEnumerator Archer_E(float SightAngle, float distance,float damage)
     {
-        yield return new WaitForEndOfFrame();
+        pos_selected = false;
+        canceled = false;
+        Vector3 pos;
+        StartCoroutine(Show_CircleIndicator(true,SightAngle,distance));
+        while(!canceled)
+        {
+            while(!pos_selected)
+            {
+                if(Input.GetMouseButton(0))
+                {
+                    destination.Clear();
+                    RaycastHit hit;
+                    bool raycastHit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out hit);
+                    if (!raycastHit)
+                        canceled = true;
+                    else
+                    {              
+                        pos = hit.point;
+                        player.transform.right = -new Vector3(pos.x - player.transform.position.x, 0, pos.z - player.transform.position.z).normalized;
+                        pos_selected = true;
+                    }
+                }
+                else if(Input.GetMouseButton(1)||Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.Q)||Input.GetKey(KeyCode.R))
+                {
+                    canceled = true;
+                    on_skill = false;
+                    break;
+                }
+                yield return new WaitForEndOfFrame();
+            }
+            if(pos_selected)
+            {
+                curState = State.STATE_SKILL;
+                skill = HotKey.E;
+                Ani_State_Change();
+                Vector3 pos_s = player.transform.GetChild(1).transform.GetChild(0).position;
+                yield return new WaitForSeconds(1.9f);
+                List<GameObject> temp = new List<GameObject>();
+                int how_many = (int)(SightAngle/10f);
+    
+                GameObject skill_arrow = Resources.Load<GameObject>("Prefabs/Skill_Arrow_small_size");
+                for(int i = 0; i < how_many; i++)
+                {
+                    GameObject a = Instantiate(skill_arrow);
+                    a.transform.position = pos_s;
+                    a.transform.forward = -player.transform.right;
+                    a.transform.Rotate(0, 10f * (i - how_many/2f),0);
+                    temp.Add(a);
+                }
+                float arrow_moved = 0f;
+                float time = 0f;
+                float speed = 15f;
+                while(arrow_moved < distance && time < 5f)
+                {
+                    float move_distance = Time.deltaTime * speed;
+                    foreach(GameObject i in temp)
+                    {
+                        i.transform.position += (i.transform.forward * move_distance);
+                    }
+                    arrow_moved += move_distance;
+                    time += Time.deltaTime;
+                    yield return new WaitForEndOfFrame();
+                }
+                for(int i = 0; i < enemies.Count; i++)
+                {
+                    Vector3 targetDir = (enemies[i].transform.position - player.transform.position).normalized;//방향 계산
+
+                    float dot = Vector3.Dot(player.transform.forward, targetDir);//내적 계산
+
+                    float theta = Mathf.Acos(dot) * Mathf.Rad2Deg;//내적과 acos을 이용하여 사이각 계산
+
+                    float far = Vector3.Distance(player.transform.position, enemies[i].transform.position);
+
+                    if(theta <= SightAngle && far < distance)
+                        enemies[i].GetComponent<Stat>().Hp -= damage;
+                }
+                foreach(GameObject i in temp)
+                    Destroy(i);
+                cool[0] = cool_max[0];
+                on_skill = false;
+                break;
+            }
+            yield return new WaitForEndOfFrame();
+        }
+        pos_selected = false;
+        canceled = false;
     }
 
     IEnumerator Archer_R()
@@ -216,12 +299,16 @@ public class Archer : Player
         canceled = false;
     }
 
-    IEnumerator Passive()
+    IEnumerator Archer_Passive()
     {
-        if(my_stat.level_up)
+        while(true)
         {
-            attackRange = 15 + my_stat.Level * 0.75f;
+            if(my_stat.level_up)
+            {
+                attackRange = 15 + (my_stat.Level-1) * 0.25f;
+                yield return new WaitForEndOfFrame();
+            }
+            yield return new WaitForEndOfFrame();
         }
-        yield return new WaitForEndOfFrame();
     }
 }
