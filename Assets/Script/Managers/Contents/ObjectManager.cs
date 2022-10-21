532a2d6c52edd107b09798cf9127ae13d85c545a
HOYOUNG LEE <tizm32@naver.com>
2022-10-21 13:33:11 +0900

    HY) ObjectManager 주석처리

--- Assets/Script/Managers/Contents/ObjectManager.cs
+++ Assets/Script/Managers/Contents/ObjectManager.cs
@@ -6,80 +6,80 @@ using UnityEngine;
 
 public class ObjectManager
 {
-    public MyPlayer MyPlayer { get; set; }
-    Dictionary<int, GameObject> _objects = new Dictionary<int, GameObject>();
+    //public MyPlayer MyPlayer { get; set; }
+    //Dictionary<int, GameObject> _objects = new Dictionary<int, GameObject>();
 
-    public void Add(PlayerInfo info, bool myPlayer = false)
-    {
-        if(myPlayer)
-        {
-            GameObject go = Managers.Resource.Instantiate("Prefabs/Character/MyWarrior");
-            go.name = info.Name;
-            _objects.Add(info.PlayerId, go);
+    //public void Add(PlayerInfo info, bool myPlayer = false)
+    //{
+    //    if(myPlayer)
+    //    {
+    //        GameObject go = Managers.Resource.Instantiate("Prefabs/Character/MyWarrior");
+    //        go.name = info.Name;
+    //        _objects.Add(info.PlayerId, go);
 
-            MyPlayer = go.GetComponent<MyPlayer>();
-            MyPlayer.Id = info.PlayerId;
-            MyPlayer.Pos = new Vector3Int(info.PosX, 1, info.PosY);
-        }
-        else
-        {
-            GameObject go = Managers.Resource.Instantiate("Prefabs/Character/Warrior");
-            go.name = info.Name;
-            _objects.Add(info.PlayerId, go);
+    //        MyPlayer = go.GetComponent<MyPlayer>();
+    //        MyPlayer.Id = info.PlayerId;
+    //        MyPlayer.Pos = new Vector3Int(info.PosX, 1, info.PosY);
+    //    }
+    //    else
+    //    {
+    //        GameObject go = Managers.Resource.Instantiate("Prefabs/Character/Warrior");
+    //        go.name = info.Name;
+    //        _objects.Add(info.PlayerId, go);
 
-            Player p = go.GetComponent<Player>();
-            p.Id = info.PlayerId;
-            p.Pos = new Vector3Int(info.PosX, 1, info.PosY);
-        }
-    }
+    //        Player p = go.GetComponent<Player>();
+    //        p.Id = info.PlayerId;
+    //        p.Pos = new Vector3Int(info.PosX, 1, info.PosY);
+    //    }
+    //}
 
-    public void Add(int id, GameObject go)
-    {
-        _objects.Add(id, go);
-    }
+    //public void Add(int id, GameObject go)
+    //{
+    //    _objects.Add(id, go);
+    //}
 
-    public void Remove(int id)
-    {
-        _objects.Remove(id);
-    }
+    //public void Remove(int id)
+    //{
+    //    _objects.Remove(id);
+    //}
 
-    public void RemoveMyPlayer()
-    {
-        if (MyPlayer == null) return;
+    //public void RemoveMyPlayer()
+    //{
+    //    if (MyPlayer == null) return;
 
-        Remove(MyPlayer.Id);
-        MyPlayer = null;
-    }
+    //    Remove(MyPlayer.Id);
+    //    MyPlayer = null;
+    //}
 
 
-    public GameObject Find(Vector3Int Pos)
-    {
-        foreach(GameObject go in _objects.Values)
-        {
-            Player player = go.GetComponent<Player>();
-            if (player == null) continue;
+    //public GameObject Find(Vector3Int Pos)
+    //{
+    //    foreach(GameObject go in _objects.Values)
+    //    {
+    //        Player player = go.GetComponent<Player>();
+    //        if (player == null) continue;
 
-            //if (player.Pos == Pos)
-            //    return go;
+    //        //if (player.Pos == Pos)
+    //        //    return go;
 
-        }
+    //    }
 
-        return null;
-    }
+    //    return null;
+    //}
 
-    public GameObject Find(Func<GameObject, bool> condition)
-    {
-        foreach (GameObject go in _objects.Values)
-        {
-            if (condition.Invoke(go))
-                return go;
-        }
+    //public GameObject Find(Func<GameObject, bool> condition)
+    //{
+    //    foreach (GameObject go in _objects.Values)
+    //    {
+    //        if (condition.Invoke(go))
+    //            return go;
+    //    }
 
-        return null;
-    }
+    //    return null;
+    //}
 
-    public void Clear()
-    {
-        _objects.Clear();
-    }
+    //public void Clear()
+    //{
+    //    _objects.Clear();
+    //}
 }
