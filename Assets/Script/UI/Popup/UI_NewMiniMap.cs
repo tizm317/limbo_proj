52d49331ecaf05bd03a276229ffba7647897c0ea
HOYOUNG LEE <tizm32@naver.com>
2022-11-03 20:06:27 +0900

    HY) 빌리지 trrain 수정(tilling 2 ?->20) / Terrain 폴더 안에 넣음 / 미니맵 프리팹 수정시작

---
+++ Assets/Script/UI/Popup/UI_NewMiniMap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UI_NewMiniMap : UI_Popup
+{
+    // MiniMap New Version
+    
+    enum Texts
+    {
+        MapNameText,
+    }
+
+    void Start()
+    {
+        Init();
+    }
+
+    public override void Init()
+    {
+        base.Init();
+
+        Bind<Text>(typeof(Texts));
+
+        // 맵 이름 초기화
+        Text _MapNameText = GetText((int)Texts.MapNameText);
+        _MapNameText.text = SceneManager.GetActiveScene().name;
+        if (_MapNameText.text.Contains("InGame"))
+            _MapNameText.text = _MapNameText.text.Substring(6);
+    }
+}
